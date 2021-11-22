4af74c2b3c552dcf12ffb64d8963e608b6e7219c
Erick Yudha Pratama Sukku <erickyudha18@gmail.com>
2021-11-22 12:36:56 +0700

    Update berita.php

--- p/berita.php
+++ p/berita.php
@@ -16,6 +16,7 @@ $newsViewData = filterWisataDataByCity($allWisataData, "medan");
     <link rel="stylesheet" href="../style/berita.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <title>News - BUS KITA</title>
+    <link rel="icon" href="../img/bus-solid.png">
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed top navigasi">
